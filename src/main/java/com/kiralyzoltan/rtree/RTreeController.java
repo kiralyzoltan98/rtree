ca51c964061ddbe96ccf2cbe41214e27976c9eb0
kiralyzoltan98 <kiralyzoltan98@gmail.com>
2025-01-19 00:08:49 +0100

    Introduced Service class. Enabled users to reuse the /history's response and search with escaped jsonData.

--- src/main/java/com/kiralyzoltan/rtree/RTreeController.java
+++ src/main/java/com/kiralyzoltan/rtree/RTreeController.java
@@ -1,84 +1,35 @@
 package com.kiralyzoltan.rtree;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kiralyzoltan.rtree.config.AppInstanceConfig;
 import com.kiralyzoltan.rtree.history.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.nio.file.DirectoryIteratorException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.sql.Timestamp;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
 public class RTreeController {
 
-    private final HistoryRepository historyRepository;
-    private final HistoryMapper historyMapper;
-    private final AppInstanceConfig appInstanceConfig;
+    private final RTreeService rTreeService;
 
-    public RTreeController(HistoryRepository historyRepository, HistoryMapper historyMapper, AppInstanceConfig appInstanceConfig) {
-        this.historyRepository = historyRepository;
-        this.historyMapper = historyMapper;
-        this.appInstanceConfig = appInstanceConfig;
+    public RTreeController(RTreeService rTreeService) {
+        this.rTreeService = rTreeService;
     }
 
     @GetMapping("/getunique")
     public List<String> getUniqueFilenames(@RequestParam String path, @RequestParam Optional<String> extension) { // TODO: implement extension filtering
-        HashMap<String, Integer> filenames = new HashMap<>();
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Path.of(path))) {
-            for (Path file : stream) {
-                String filename = file.getFileName().toString();
-                filenames.put(filename, filenames.getOrDefault(filename, 0) + 1);
-            }
-        } catch (IOException | DirectoryIteratorException e) {
-            log.error("Error could not read directory: {}", e.getMessage());
-        }
-
-        List<String> uniqueFilenames = filenames.keySet().stream().filter(k -> filenames.get(k) == 1).toList();
-
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            //TODO: get the appinstance/appid from properties
-            historyRepository.save(new History(appInstanceConfig.getInstanceName(), new Timestamp(System.currentTimeMillis()), mapper.writeValueAsString(uniqueFilenames)));
-        } catch (JsonProcessingException e) {
-            log.error("Error could not save history: {}", e.getMessage());
-        }
-
-        return uniqueFilenames;
+        return rTreeService.getUniqueFilenames(path, extension);
     }
 
     @GetMapping("/history")
     public List<HistoryResponse> getHistory(@RequestParam Optional<String> username,
                                             @RequestParam Optional<Timestamp> createdAt,
-                                            @RequestParam Optional<String> jsonData)
+                                            @RequestParam Optional<String> jsonData)    // The json data would fit better in the request body
     {
-        Specification<History> spec = Specification.where(null);
-
-        if (username.isPresent()) {
-            spec = spec.and(HistorySpecifications.hasUsername(username.get()));
-        }
-        if (createdAt.isPresent()) {
-            spec = spec.and(HistorySpecifications.hasCreatedAt(createdAt.get()));
-        }
-        if (jsonData.isPresent()) {
-            spec = spec.and(HistorySpecifications.hasJsonData(jsonData.get()));
-        }
-
-        return historyRepository.findAll(spec).stream()
-                .map(historyMapper::toResponse)
-                .collect(Collectors.toList());
+        return rTreeService.getHistory(username, createdAt, jsonData);
     }
 }
---
+++ src/main/java/com/kiralyzoltan/rtree/RTreeService.java
@@ -0,0 +1,84 @@
+package com.kiralyzoltan.rtree;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kiralyzoltan.rtree.config.AppInstanceConfig;
+import com.kiralyzoltan.rtree.history.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class RTreeService {
+
+    private final AppInstanceConfig appInstanceConfig;
+    private final HistoryRepository historyRepository;
+    private final HistoryMapper historyMapper;
+
+    public RTreeService(AppInstanceConfig appInstanceConfig, HistoryRepository historyRepository, HistoryMapper historyMapper) {
+        this.appInstanceConfig = appInstanceConfig;
+        this.historyRepository = historyRepository;
+        this.historyMapper = historyMapper;
+    }
+
+    @GetMapping("/getunique")
+    public List<String> getUniqueFilenames(@RequestParam String path, @RequestParam Optional<String> extension) { // TODO: implement extension filtering
+        HashMap<String, Integer> filenames = new HashMap<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Path.of(path))) {
+            for (Path file : stream) {
+                String filename = file.getFileName().toString();
+                filenames.put(filename, filenames.getOrDefault(filename, 0) + 1);
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            log.error("Error could not read directory: {}", e.getMessage());
+        }
+
+        List<String> uniqueFilenames = filenames.keySet().stream().filter(k -> filenames.get(k) == 1).toList();
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            historyRepository.save(new History(appInstanceConfig.getInstanceName(), new Timestamp(System.currentTimeMillis()), mapper.writeValueAsString(uniqueFilenames)));
+        } catch (JsonProcessingException e) {
+            log.error("Error could not save history: {}", e.getMessage());
+        }
+
+        return uniqueFilenames;
+    }
+
+    public List<HistoryResponse> getHistory(@RequestParam Optional<String> username,
+                                            @RequestParam Optional<Timestamp> createdAt,
+                                            @RequestParam Optional<String> jsonData)
+    {
+        Specification<History> spec = Specification.where(null);
+
+        if (username.isPresent()) {
+            spec = spec.and(HistorySpecifications.hasUsername(username.get()));
+        }
+        if (createdAt.isPresent()) {
+            spec = spec.and(HistorySpecifications.hasCreatedAt(createdAt.get()));
+        }
+        if (jsonData.isPresent()) {
+            // This is a quick workaround to enable users to reuse the json data they got from the response
+            String unescapedJsonData = jsonData.get().replace("\\", "");
+            spec = spec.and(HistorySpecifications.hasJsonData(unescapedJsonData));
+        }
+
+        return historyRepository.findAll(spec).stream()
+                .map(historyMapper::toResponse)
+                .collect(Collectors.toList());
+    }
+}
